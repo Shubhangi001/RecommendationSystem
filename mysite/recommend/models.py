f8712169fa1b1dc8dec91ce9c36df20eeea40b97
Shubhangi001 <shubhangispc@gmail.com>
2022-05-25 19:13:48 +0530

    movie table csv file imported in database

--- mysite/recommend/models.py
+++ mysite/recommend/models.py
@@ -1,13 +1,26 @@
+from platform import release
+from typing_extensions import runtime
 from django.db import models
-
+from django.utils.translation import gettext as _
 # Create your models here.
-# class Movie(models.Model):
-#     original_title=models.CharField(_("original_title"),max_length=255)
-#     title=models.CharField(_("title"),max_length=255)
-#     genres=models.CharField(_("genres"),max_length=255)
-#     homepage=models.CharField(_("homepage"),max_length=255)
-#     keywords=models.CharField(_("keywords"),max_length=255)
-#     cast=models.CharField(_("cast"),max_length=255)
-#     director=models.CharField(_("director"),max_length=255)
-#     vote_average=models.FloatField(_("vote_average") `                                                                                                               `)
+class Movie(models.Model):                                                                                                                                                                                                    
+    genres=models.CharField(_("genres"),max_length=255)
+    homepage=models.CharField(_("homepage"),max_length=255)
+    movie_id=models.IntegerField(_("movie_id"))
+    keywords=models.CharField(_("keywords"),max_length=255)
+    original_language=models.CharField(_("original_language"),max_length=20)
+    original_title=models.CharField(_("original_title"),max_length=255)
+    overview=models.CharField(_("overview"),max_length=1255)
+    popularity=models.FloatField(_("popularity"))
+    production_countries=models.CharField(_("production_countries"),max_length=1255)
+    release_date=models.DateField(_("release_date"))
+    duration=models.IntegerField(_("duration"))
+    spoken_language=models.CharField(_("spoken_language"),max_length=1255)
+    tagline=models.CharField(_("tagline"),max_length=1255)
+    title=models.CharField(_("title"),max_length=255)
+    vote_average=models.FloatField(_("vote_average"))
+    vote_count=models.IntegerField(_("vote_count"))
+    cast=models.CharField(_("cast"),max_length=1255)
+    director=models.CharField(_("director"),max_length=255)
+
                                                                                                                                                      
